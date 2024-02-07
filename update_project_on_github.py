7c8037de5510c820bde7cae924c28fe29556eda3
JonasHell <jonas.hellgoth@gmx.de>
2024-02-07 09:19:49 +0100

    remove comments

--- update_project_on_github.py
+++ update_project_on_github.py
@@ -17,25 +17,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # potential merge conflicts need to be saved manually on the command line
+    # potential merge conflicts need to be solved manually on the command line
     main()
-
-
-# git pull before I change anything (do this in code)
-# git stage
-# git commit
-# git pull
-# solve potential merge conflicts
-# git push
-
-# either people need a gihub account and I add them as a collaborator
-# or everyone gets the password to the deylab github account
-
-# I should git pull before I add files
-# check if git repo is here (must be)
-# find new/modified files and satge them
-    # check for gitignore
-# commit
-    # use certain commit message
-# push
-    # is already pulled?
