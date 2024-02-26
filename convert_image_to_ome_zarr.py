0d0b7b0c1aafa04af3534187fea2bb5ec9cfee08
JonasHell <jonas.hellgoth@gmx.de>
2024-02-26 11:33:24 +0100

    change default chunk size to 96 cubed

--- convert_image_to_ome_zarr.py
+++ convert_image_to_ome_zarr.py
@@ -1,25 +1,29 @@
-
 import argparse
 import os
-import subprocess
 import shlex
+import subprocess
 
 
 def generate_zarr_file_path(input_file_path):
-    file_name = os.path.split(input_file_path)[1].split('.')[0]
+    file_name = os.path.split(input_file_path)[1].split(".")[0]
     file_name += ".ome.zarr"
     output_file_path = os.path.join(os.getcwd(), "tmp", file_name)
     return output_file_path
 
 
-def convert_to_ome_zarr(input_file_path, overwrite=False):
+def convert_to_ome_zarr(input_file_path, overwrite=False, chunk_size=(96, 96, 96)):
     output_file_path = generate_zarr_file_path(input_file_path)
     if os.path.exists(output_file_path) and not overwrite:
         return output_file_path
     # use subprocess to call bioformats2raw
-    cmd = f'bioformats2raw {input_file_path} {output_file_path} ' \
-        '--target-min-size 32 ' \
-        '--scale-format-string "%2$d/"'
+    cmd = (
+        f"bioformats2raw {input_file_path} {output_file_path} "
+        "--target-min-size 32 "
+        '--scale-format-string "%2$d/" '
+        f"-z {chunk_size[0]} "
+        f"-h {chunk_size[1]} "
+        f"-w {chunk_size[2]}"
+    )
     cmd = shlex.split(cmd)
     subprocess.run(cmd)
     return output_file_path
