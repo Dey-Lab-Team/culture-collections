db7dfae73c0c43a7fd8e3ef43918155433480c3a
JonasHell <jonas.hellgoth@gmx.de>
2024-02-26 10:49:13 +0100

    add new images

--- update_project_on_github.py
+++ update_project_on_github.py
@@ -22,7 +22,13 @@ def push(remote: str = "origin", branch: str = "main"):
     )
     if push_process.stderr:
         print(push_process.stderr)
-    return not push_process.stderr
+    is_pushed = not push_process.stderr
+    # git writes output to stderr, so we need to check for the desired output
+    if push_process.stderr.startswith(
+        "To github.com:Dey-Lab-Team/culture-collections.git"
+    ):
+        is_pushed = True
+    return is_pushed
 
 
 def sync_with_remote():
