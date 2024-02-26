ca5af58514cfe1ffdb192d96441d2ea050d3b738
JonasHell <jonas.hellgoth@gmx.de>
2024-02-26 10:40:00 +0100

    add new images

--- update_project_on_github.py
+++ update_project_on_github.py
@@ -10,22 +10,14 @@ def pull(rebase: bool = False) -> bool:
     command = ["git", "pull"]
     if rebase:
         command.append("--rebase")
-    pull_process = subprocess.run(
-        command,
-        capture_output=True,
-        text=True
-    )
+    pull_process = subprocess.run(command, capture_output=True, text=True)
     if pull_process.stderr:
         print(pull_process.stderr)
     return not pull_process.stderr
 
 
 def push():
-    push_process = subprocess.run(
-        ["git", "push"],
-        capture_output=True,
-        text=True
-    )
+    push_process = subprocess.run(["git", "push"], capture_output=True, text=True)
     if push_process.stderr:
         print(push_process.stderr)
     return not push_process.stderr
@@ -37,6 +29,9 @@ def sync_with_remote():
         return False
     is_pushed = push()
     return is_pushed
+
+
+# make changes to test
 
 
 def main():
