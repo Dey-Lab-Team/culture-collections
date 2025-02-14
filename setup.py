8b0931f645e23fa3e0dd80386ce3157e6f7859cb
JonasHell <jonas.hellgoth@gmx.de>
2025-02-14 15:48:08 +0100

    add setup.py to make package installable, since it is used for other projet

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="culture-collections",
+    version="0.0.1",
+    description="Python files to create a MoBIE project for culture collections",
+    author="Jonas Hellgoth",
+    author_email="jonas.hellgoth@embl.de",
+    packages=["culture-collections"],  # same as name
+    install_requires=["numpy", "elf", "mobie"],  # external packages as dependencies
+)
