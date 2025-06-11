fc51e940aa606d0fee5980bf776e4d69ce8ff245
JonasHell <jonas.hellgoth@gmx.de>
2025-06-11 14:51:52 +0200

    correct renaming ans check again

--- culture_collections/notebooks/correct_renaming_of_file_on_s3.py
+++ culture_collections/notebooks/correct_renaming_of_file_on_s3.py
@@ -1,27 +1,51 @@
+import os
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 from tqdm import tqdm
 
 
 def main():
-    with open("file_name_map.csv", "r") as f:
-        new_file_names = [line.split(",")[1].strip() for line in f.readlines()]
+    with open(os.path.dirname(__file__) + "/file_name_map.csv", "r") as f:
+        file_name_map = {
+            line.split(",")[0].strip(): line.split(",")[1].strip()
+            for line in f.readlines()
+        }
 
-    processes: list[subprocess.Popen] = []  # type: ignore
-    for new_file_name in new_file_names:
+    # sort for the ones that did not work
+    p = "/home/hellgoth/culture-collections/data/single_volumes/images/ome-zarr/"
+    new_dict: dict[str, str] = {}
+    for old_file_name, new_file_name in file_name_map.items():
+        if old_file_name + ".ome.zarr" in os.listdir(p + new_file_name + ".ome.zarr/"):
+            new_dict[old_file_name] = new_file_name
+    print("Number of files that did not work:", len(new_dict))
+
+    pbar = tqdm(
+        total=len(new_dict),
+        desc="Renaming files on S3",
+        unit="file",
+    )
+
+    def rename_file(old_file_name: str, new_file_name: str):
         cmd = [
             "mc",
             "mv",
             "-r",
             "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/"
-            + f"{new_file_name}.ome.zarr/{new_file_name}.zarr/",
+            + f"{new_file_name}.ome.zarr/{old_file_name}.ome.zarr/",
             "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/"
             + f"{new_file_name}.ome.zarr/",
         ]
-        processes.append(subprocess.Popen(cmd))  # type: ignore
-
-    for p in tqdm(processes):  # type: ignore
+        p = subprocess.Popen(cmd)
         p.wait()
+        pbar.update(1)
+
+    with ThreadPoolExecutor(4) as ex:
+        ex.map(
+            rename_file,
+            new_dict.keys(),
+            new_dict.values(),
+        )
 
 
 if __name__ == "__main__":
