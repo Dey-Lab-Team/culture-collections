4905dfd75f4632ba52b8de4fedf8e8a35f0fe77e
JonasHell <jonas.hellgoth@gmx.de>
2025-06-12 17:05:55 +0200

    improve script

--- culture_collections/notebooks/download_and_zip_files.sh
+++ culture_collections/notebooks/download_and_zip_files.sh
@@ -1,6 +1,9 @@
+c=1
 while read f; do
-    # mc cp -r "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/$f" "/scratch/hellgoth/planexm_zips"
+    mc cp -r "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/$f" "/scratch/hellgoth/planexm_zips"
     echo "Downloaded: $f"
     zip -q -r "/scratch/hellgoth/planexm_zips/$f.zip" "/scratch/hellgoth/planexm_zips/$f"
     echo "Zipped: $f"
+    echo "Done: $c / 1186"
+    c=$((c + 1))
 done < ./culture_collections/notebooks/all_files.txt
