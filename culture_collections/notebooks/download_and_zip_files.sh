c3155daf04ac64cbcce89e3da0afa41c855db15a
JonasHell <jonas.hellgoth@gmx.de>
2025-06-18 09:49:55 +0200

    new ideas for script for potential future use

--- culture_collections/notebooks/download_and_zip_files.sh
+++ culture_collections/notebooks/download_and_zip_files.sh
@@ -1,11 +1,27 @@
 c=1
-while read f; do
-    mc cp -q -r "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/$f" "/scratch/hellgoth/planexm_zips"
-    echo "Downloaded: $f"
-    zip -q -r "/scratch/hellgoth/planexm_zips/$f.zip" "/scratch/hellgoth/planexm_zips/$f"
-    echo "Zipped: $f"
-    rm -rf "/scratch/hellgoth/planexm_zips/$f"
-    echo "Removed: $f"
-    echo "Done: $c / 1186"
-    c=$((c + 1))
-done < ./culture_collections/notebooks/all_files.txt
+cat ./culture_collections/notebooks/all_files.txt | while read file; do
+    if [ -e "/mnt/embl/scratch/hellgoth/planexm_zips/$file.zip" ]; then
+        echo "File already exists: $file.zip"
+        echo "Done: $c / 1186"
+        c=$((c + 1))
+    else
+        # mc cp -q -r "culcol_s3_rw/culture-collections/data/single_volumes/images/ome-zarr/$file" "/scratch/hellgoth/planexm_zips"
+        # echo "Downloaded: $file"
+        # zip -q -r "/scratch/hellgoth/planexm_zips/$file.zip" "/scratch/hellgoth/planexm_zips/$file"
+        # echo "Zipped: $file"
+        # rm -rf "/scratch/hellgoth/planexm_zips/$f"
+        # echo "Removed: $file"
+        echo "Downloading and zipping: $file"
+        echo "Done: $c / 1186"
+        c=$((c + 1))
+    fi
+done
+
+# file=rcc6324_pfa_nhs-tub-npc-dna_20231121_fm_04.ome.zarr
+# echo "/mnt/embl/scratch/hellgoth/planexm_zips/$file.zip"
+# if [ -e "/mnt/embl/scratch/hellgoth/planexm_zips/$file.zip" ]; then
+#     echo "File already exists: $file.zip"
+# else
+#     echo "Downloading and zipping: $file"
+#     echo "Done: 1 / 1"
+# fi
